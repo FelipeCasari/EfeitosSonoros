3a69342e04a20e082a3fdbe1b344119e4cdbf07c
FelipeCasari <110491590+FelipeCasari@users.noreply.github.com>
2023-11-06 17:35:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
+function tocaSomAplauso(){
+    document.querySelector("#som_tecla_Aplauso").play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla')
+listaDeTeclas[2].onclick = tocaSomAplauso
+
+let contador = 0;
+//Estrutura de Repetição - Enquanto
+while (contador<9){
+    listaDeTeclas[0].onclick = tocaSomAplauso
+    contador = 1;
+}
