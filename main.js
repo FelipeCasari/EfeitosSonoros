9b42b85e707f5ec4c51ea00307dff969e22c231c
FelipeCasari <110491590+FelipeCasari@users.noreply.github.com>
2023-11-20 17:19:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+function tocaSomAplausos(){
+    document.querySelector("#som_tecla_aplausos").play();   
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    document.querySelector(idElementoAudio).play();   
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[2].onclick = tocaSomAplausos
 
 let contador = 0;
-//Estrutura de Repetição - Enquanto
-while (contador<listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomAplauso
+//Estrutura de repetição - Enquanto
+while(contador<9){
+    listaDeTeclas[0].onclick = tocaSomAplausos;
     contador = 1;
+while(contador<listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    tecla.onclick = function(){
+    const idAudio = `#som_${instrumento}`;
+    }
+    contador = contador + 1;
+    console.log(instrumento);
 }
